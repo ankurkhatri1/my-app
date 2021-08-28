7252952c3db16931b8ff8b0bb0492de38605f30f
ankurkhatri1 <71034870+ankurkhatri1@users.noreply.github.com>
2021-08-28 16:01:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Textform from './components/Textform';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+//import About from './components/About';
+//import {
+  //BrowserRouter as Router,
+  //Switch,
+  //Route,
+  //Link
+//} from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setalert] = useState(null)
+
+  const showalert=(message,type)=>{
+    setalert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setalert(null)
+    }, 1000);
+  }
+  
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showalert('dark mode has been enabled','success')
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showalert('light mode has been enabled','success')
+    }
+  }
+  
+  return (
+    <>
+    {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+    {/* <Navbar/> */}
+    {/*<Router>*/}
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}  />
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    {/*<Switch>*/}
+          {/*<Route  exec path="/about">
+            <About />
+  </Route>*/}
+         
+          {/*<Route exec path="/">*/}
+          <Textform heading="Enter the text to analyze below" mode={mode}/>
+            
+         {/* </Route>*/}
+        {/*</Switch>*/}
+        </div>
+        {/*</Router>*/}
+    
+    {/*<About/>*/}
+   
+    </> 
+  );
+  }
+
+export default App
+  
---
+++ src/components/Textform.js
@@ -0,0 +1,54 @@
+import React,{useState} from 'react'
+
+
+
+export default function Textform(props) {
+    const handleUpClick =()=>{
+            console.log("upperCase was clicked"+text)
+            let newText=text.toUpperCase()
+            
+            settext(newText)
+           
+            
+    }
+    const handlowclick =()=>{
+        console.log("upperCase was clicked"+text)
+        let newText=text.toLowerCase()
+        
+        settext(newText)}
+
+        const clear =()=>{
+            
+            settext(" ")
+        }
+  
+
+    const handleOnChange =(event)=>{
+        console.log("on change")
+        settext(event.target.value)
+}
+    const [text, settext] = useState("Enter text here");
+    
+    return (
+    <>
+        <div className="container">
+
+<div className="mb-3">
+    <h1>{props.heading}</h1>
+  
+  <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+</div>
+<button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+<button className="btn btn-primary" onClick={handlowclick}>Convert to Lowercase</button>
+<button className="btn btn-primary" onClick={clear}>Cleare your text</button>
+
+        </div>
+        <div className="container my-2">
+            <h1>Your text summary</h1>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <h2>preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
+    )
+}
